64457ae267287cc50f88a7de2eb9f920a719cbcc
Bjoey <wisnuhutama@outlook.com>
2022-04-07 13:32:03 +0800

    commitnew1

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { LockIcon } from '@chakra-ui/icons'
-import { Box, Button ,Center ,Stack} from '@chakra-ui/react'
+import { Box, Button ,Center ,Stack , Kbd, Text} from '@chakra-ui/react'
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
 import { auth } from '../firebaseconfig'
 
@@ -16,6 +16,7 @@ function Login() {
     </Head>
 
     <Center h="100vh">
+      <Text as="samp" p={5}>Website ini dibuat oleh <Kbd>Wisnu</Kbd> / <Kbd>Bjoey</Kbd></Text>
         <Stack align="center" bgColor="gray.700" p={16} rounded="3xl" spacing={12}>
     <Box boxShadow="dark-lg" bgColor="blue.500" w="fit-content" bgGradient='linear(red.100 0%, orange.100 25%, yellow.100 50%)' p={5} rounded="xl">
     <LockIcon w="100px" h="100px" color="black" />
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -82,7 +82,7 @@ const Chat = () => {
         </Head>
         <Sidebar />
 
-        <Flex bg="black" flex={1} direction="column">
+        <Flex bg="white" flex={1} direction="column">
             <Topbar email={getOtherEmail(chat?.users, user)} />
         <Flex flex={1} direction="column" pt={4} mx={5} overflowX="hidden" sx={{'&::-webkit-scrollbar': {
       width: '16px',
